44bbcf8e8a2e23a21d8e5d7fa34e0115a87b01ab
Sarina <sam562100@gmail.com>
2024-01-24 21:19:58 -0800

    finished phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,12 +5,15 @@ const express = require('express') //1
 //2. User Login API Route
 const { Op } = require('sequelize'); //2 import sequelize operators
 const bcrypt = require('bcryptjs'); //2 import bcrypt
+
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth'); //2
+const { User } = require('../../db/models'); //2
+
 //3. Validating Login Request Body
 const { check } = require('express-validator'); //3
 const { handleValidationErrors } = require('../../utils/validation'); //3
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth'); //2
-const { User } = require('../../db/models'); //2
 const router = express.Router();//1
 
 //3. validateLogin middleware that check the keys and validates them
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,8 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+
 const apiRouter = require('./api');
-
-
-
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
