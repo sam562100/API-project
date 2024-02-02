308f76ed9ca218edf47c5d913be887ba59479ac1
Sarina <sam562100@gmail.com>
2024-02-01 22:16:55 -0800

    started create a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -11,5 +11,67 @@ const { User } = require('../../db/models'); //2
 //3. Validating Login Request Body
 const { check } = require('express-validator'); //3
 const { handleValidationErrors } = require('../../utils/validation'); //3
+const { Spot, User, Sequelize } = require('../../db/models');
 
 const router = express.Router();//1
+
+const { requireAuth } = require("../../utils/auth");
+const { environment } = require("../../config");
+const isProduction = environment === "production";
+
+const testAuthorization = async (req, res, next) => {
+	const { id: userId } = req.user;
+	const { id: spotId } = req.params;
+
+	try {
+		const mySpot = await Spot.findByPk(spotId);
+
+		if (!mySpot){
+			res.status(404).json({"message": "Spot couldn't be found"})
+		}
+
+		const { ownerId } = mySpot;
+
+		if (Number(userId) !== Number(ownerId)) throw new Error("Forbidden");
+	} catch (err) {
+		return next(err);
+	}
+	return next();
+};
+
+//Create a Spot
+router.post(
+    '/',
+    async (req, res) => {
+        const { id: userId } = req.user;
+
+        const { address, city, state, country, lat, lng, name, description, price } = req.body;
+        const spot = await Spot.create({
+            ownerId: userId,
+            address: address,
+            city: city,
+            state: state,
+            country: country,
+            lat: lat,
+            lng: lng,
+            name: name,
+            description: description,
+            price: price,
+        });
+
+        return res.status(201).json({
+            id: userId,
+            address: address,
+            city: city,
+            state: state,
+            country: country,
+            lat: lat,
+            lng: lng,
+            name: name,
+            description: description,
+            price: price,
+        })
+    }
+)
+
+module.exports = router; //1
